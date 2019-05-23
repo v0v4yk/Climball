5f3ddec63d6721fdc1649d56a357f35d26eb657f
v0v4yk <50955132+v0v4yk@users.noreply.github.com>
2019-05-23 20:29:22 +0700

    Create Menu.cs

---
+++ Menu.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace делаем_норм_скалодром__2_
+{
+    /// <summary>
+    /// Interaction logic for Menu.xaml
+    /// </summary>
+    public partial class Menu : Window
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Versus play = new Versus();
+            play.VSmaxscore = int.Parse(VSmaxscore.Text);
+            play.VSmaxwins = int.Parse(VSmaxwin.Text);
+            play.Show();
+            Close();
+        }
+
+        private void RichTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
